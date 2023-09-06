945afba60a7ad18bf2553e373f9bc49468d9f766
Kutin, Sergey <skutin@prosv.ru>
2023-09-06 15:17:46 +0300

    chunk size = 1

--- data/seed.mjs
+++ data/seed.mjs
@@ -16,7 +16,7 @@ console.log('Connected to MongoDB cluster')
 //set chunksize = 4MB
 const configDb = client.db('config')
 const settings = configDb.collection('settings')
-await settings.updateOne({_id: "chunksize"}, {$set: {_id: "chunksize", value: 4}}, {upsert: true})
+await settings.updateOne({_id: "chunksize"}, {$set: {_id: "chunksize", value: 1}}, {upsert: true})
 
 const socialDb = client.db('social')
 const dbUsers = socialDb.collection('users')
@@ -25,36 +25,36 @@ const dbFriends = socialDb.collection('friends')
 const dbPosts = socialDb.collection('posts')
 
 // seed users
-console.time('users loaded successfully')
-
-const chunk_size = 10000
-
-const peopleData = fs.readFileSync('../data/people.txt', {encoding: 'utf8'})
-const peopleLines = peopleData.split('\n')
-
-await dbUsers.drop()
-// for (let i = 0; i < peopleLines.length; i += chunk_size) {
-for (let i = 0; i < chunk_size; i += chunk_size) {
-    const chunk = peopleLines.slice(i, i + chunk_size)
-    const userObjects = chunk.map(line => {
-        const [name, age, city] = line.split(',')
-        const [second_name, first_name] = name.split(' ')
-        return {
-            password: crypto.createHash('sha256').update('12345', 'utf8').digest(),
-            second_name,
-            first_name,
-            age,
-            city
-        }
-    })
-
-    await dbUsers.insertMany(userObjects)
-    console.log(i)
-}
-
-await dbUsers.createIndex({city: 1})
-
-console.timeEnd('users loaded successfully')
+// console.time('users loaded successfully')
+//
+// const chunk_size = 10000
+//
+// const peopleData = fs.readFileSync('../data/people.txt', {encoding: 'utf8'})
+// const peopleLines = peopleData.split('\n')
+//
+// await dbUsers.drop()
+// // for (let i = 0; i < peopleLines.length; i += chunk_size) {
+// for (let i = 0; i < chunk_size; i += chunk_size) {
+//     const chunk = peopleLines.slice(i, i + chunk_size)
+//     const userObjects = chunk.map(line => {
+//         const [name, age, city] = line.split(',')
+//         const [second_name, first_name] = name.split(' ')
+//         return {
+//             password: crypto.createHash('sha256').update('12345', 'utf8').digest(),
+//             second_name,
+//             first_name,
+//             age,
+//             city
+//         }
+//     })
+//
+//     await dbUsers.insertMany(userObjects)
+//     console.log(i)
+// }
+//
+// await dbUsers.createIndex({city: 1})
+//
+// console.timeEnd('users loaded successfully')
 
 const messageData = fs.readFileSync('../data/posts.txt', {encoding: 'utf8'})
 const messageLines = messageData.split('\n')
