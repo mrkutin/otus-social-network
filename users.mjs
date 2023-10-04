8c334262d01dfce1cb191a21ba16875b95664282
Kutin, Sergey <skutin@prosv.ru>
2023-10-04 18:29:46 +0300

    haproxy working

--- users.mjs
+++ users.mjs
@@ -17,36 +17,43 @@ try {
     console.error('error connecting: ' + err.stack)
 }
 
-// const create = async user => {
-//     if (!connection) {
-//         throw new Error('База данных не доступна')
-//     }
-//     const user_id = uuid()
-//     const statement = `INSERT INTO users (id, password, first_name, second_name) VALUES('${user_id}', SHA2('${user.password}', 256)   , '${user.first_name || ''}', '${user.second_name || ''}');`
-//     await connection.execute(statement)
-//     return user_id
-// }
-//
-// const authenticate = async (id, password) => {
-//     if (!connection) {
-//         throw new Error('База данных недоступна')
-//     }
-//     const statement = `SELECT id FROM users WHERE id = '${id}' AND password = SHA2('${password}', 256);`
-//     const result = await connection.execute(statement)
-//
-//     if (result?.[0]?.[0]) {
-//         return uuid()
-//     }
-//
-//     return null
-// }
-//
-// const get = async id => {
-//     const statement = `SELECT id, first_name, second_name, age, birthdate, biography, city FROM users WHERE id = '${id}';`
-//     const result = await connection.execute(statement)
-//
-//     return result?.[0]?.[0] || null
-// }
+const create = async user => {
+    const user_id = uuid()
+    const statement = `INSERT INTO users (id, password, first_name, second_name) VALUES('${user_id}', SHA2('${user.password}', 256)   , '${user.first_name || ''}', '${user.second_name || ''}');`
+
+    const connection = await pool.getConnection()
+    await connection.execute(statement)
+    await pool.releaseConnection(connection)
+
+    return user_id
+}
+
+const authenticate = async (id, password) => {
+    if (!connection) {
+        throw new Error('База данных недоступна')
+    }
+    const statement = `SELECT id FROM users WHERE id = '${id}' AND password = SHA2('${password}', 256);`
+
+    const connection = await pool.getConnection()
+    const result = await connection.execute(statement)
+    await pool.releaseConnection(connection)
+
+    if (result?.[0]?.[0]) {
+        return uuid()
+    }
+
+    return null
+}
+
+const get = async id => {
+    const statement = `SELECT id, first_name, second_name, age, birthdate, biography, city FROM users WHERE id = '${id}';`
+
+    const connection = await pool.getConnection()
+    const result = await connection.execute(statement)
+    await pool.releaseConnection(connection)
+
+    return result?.[0]?.[0] || null
+}
 
 const search = async (first_name, second_name) => {
     const like_clauses = []
@@ -57,12 +64,12 @@ const search = async (first_name, second_name) => {
         like_clauses.push(`second_name LIKE '${second_name}%'`)
     }
 
-    const connection = await pool.getConnection()
     const statement = `SELECT id, first_name, second_name, age, birthdate, biography, city FROM users WHERE ${like_clauses.join(' AND ')};`
+
+    const connection = await pool.getConnection()
     const result = await connection.execute(statement)
     await pool.releaseConnection(connection)
 
     return result?.[0] || []
 }
-// export default {create, authenticate, get, search}
-export default {search}
+export default {create, authenticate, get, search}
