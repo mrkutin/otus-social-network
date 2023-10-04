ee171e61a301c5f72a9ca0dd103e37e233d996db
Kutin, Sergey <skutin@prosv.ru>
2023-10-04 18:20:35 +0300

    haproxy working

--- users.mjs
+++ users.mjs
@@ -9,53 +9,46 @@ const config = {
     password: 'topsecret'
 }
 
-let connection
+let pool
 
 try {
-    connection = await mysql.createConnection(config)
+    pool = await mysql.createPool(config)
 } catch (err) {
     console.error('error connecting: ' + err.stack)
 }
 
-const create = async user => {
-    if (!connection) {
-        throw new Error('База данных не доступна')
-    }
-    const user_id = uuid()
-    const statement = `INSERT INTO users (id, password, first_name, second_name) VALUES('${user_id}', SHA2('${user.password}', 256)   , '${user.first_name || ''}', '${user.second_name || ''}');`
-    await connection.execute(statement)
-    return user_id
-}
-
-const authenticate = async (id, password) => {
-    if (!connection) {
-        throw new Error('База данных недоступна')
-    }
-    const statement = `SELECT id FROM users WHERE id = '${id}' AND password = SHA2('${password}', 256);`
-    const result = await connection.execute(statement)
-
-    if (result?.[0]?.[0]) {
-        return uuid()
-    }
-
-    return null
-}
-
-const get = async id => {
-    if (!connection) {
-        throw new Error('База данных недоступна')
-    }
-    const statement = `SELECT id, first_name, second_name, age, birthdate, biography, city FROM users WHERE id = '${id}';`
-    const result = await connection.execute(statement)
-
-    return result?.[0]?.[0] || null
-}
+// const create = async user => {
+//     if (!connection) {
+//         throw new Error('База данных не доступна')
+//     }
+//     const user_id = uuid()
+//     const statement = `INSERT INTO users (id, password, first_name, second_name) VALUES('${user_id}', SHA2('${user.password}', 256)   , '${user.first_name || ''}', '${user.second_name || ''}');`
+//     await connection.execute(statement)
+//     return user_id
+// }
+//
+// const authenticate = async (id, password) => {
+//     if (!connection) {
+//         throw new Error('База данных недоступна')
+//     }
+//     const statement = `SELECT id FROM users WHERE id = '${id}' AND password = SHA2('${password}', 256);`
+//     const result = await connection.execute(statement)
+//
+//     if (result?.[0]?.[0]) {
+//         return uuid()
+//     }
+//
+//     return null
+// }
+//
+// const get = async id => {
+//     const statement = `SELECT id, first_name, second_name, age, birthdate, biography, city FROM users WHERE id = '${id}';`
+//     const result = await connection.execute(statement)
+//
+//     return result?.[0]?.[0] || null
+// }
 
 const search = async (first_name, second_name) => {
-    if (!connection) {
-        throw new Error('База данных недоступна')
-    }
-
     const like_clauses = []
     if(first_name) {
         like_clauses.push(`first_name LIKE '${first_name}%'`)
@@ -64,9 +57,12 @@ const search = async (first_name, second_name) => {
         like_clauses.push(`second_name LIKE '${second_name}%'`)
     }
 
+    const connection = await pool.getConnection()
     const statement = `SELECT id, first_name, second_name, age, birthdate, biography, city FROM users WHERE ${like_clauses.join(' AND ')};`
     const result = await connection.execute(statement)
+    await pool.releaseConnection(connection)
 
     return result?.[0] || []
 }
-export default {create, authenticate, get, search}
+// export default {create, authenticate, get, search}
+export default {search}
